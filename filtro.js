c5307b4f3282f595d7d57d2a791d4f587dfe98c2
Kassandra Dominguez <kassdomlara02@gmail.com>
2023-06-25 13:28:05 -0600

    se hacen correciones en los documentos para mostar los productos filtrados

--- filtro.js
+++ filtro.js
@@ -1,4 +1,4 @@
-// Tenemos un li de productos
+// Tenemos un li de productos en un arreglo
 
 const productos = [
   {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
@@ -8,12 +8,13 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
+const li = document.getElementById("lista-de-productos") // se corrige la sintaxis para llamar al elemento lista
 const $i = document.querySelector('.input');
 
+// el ciclo for es para que se muestre en el html todo el catalogo
 for (let i = 0; i < productos.length; i++) {
   var d = document.createElement("div")
-  d.classList.add("producto")
+  d.classList.add("producto") // esto le agrega una clase al div
 
   var ti = document.createElement("p")
   ti.classList.add("titulo")
@@ -25,39 +26,48 @@ for (let i = 0; i < productos.length; i++) {
   d.appendChild(ti)
   d.appendChild(imagen)
 
-  li.appendChild(d)
+  li.appendChild(d);
 }
 
-displayProductos(productos)
-const botonDeFiltro = document.querySelector("button");
+function displayProductos(productos) {// le falta la palabra function y error en la sintaxis de la funcion, faltan las llaves
+const botonDeFiltro = document.querySelector(".button"); // se agrega un punto a la sintaxis para llamar a la clase
 
 botonDeFiltro.onclick = function() {
   while (li.firstChild) {
     li.removeChild(li.firstChild);
-  }
+  };
 
   const texto = $i.value;
   console.log(texto);
-  const productosFiltrados = filtrado(productos, texto );
-
-  for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
+  const productosFiltrados = filtro(productos, texto);
+  console.log(productosFiltrados);
   
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
+// el ciclo for sirve para mostar en el html la info de los productos filtrados.
+  for (let i = 0; i < productosFiltrados.length; i++) {
+    var d = document.createElement("div");
+    d.classList.add("producto");
+  
+    var ti = document.createElement("p");
+    ti.classList.add("titulo");
+    ti.textContent = productosFiltrados[i].nombre;
     
     var imagen = document.createElement("img");
     imagen.setAttribute('src', productosFiltrados[i].img);
   
-    d.appendChild(ti)
-    d.appendChild(imagen)
+    d.appendChild(ti);
+    d.appendChild(imagen);
   
-    li.appendChild(d)
+    li.appendChild(d);
   }
-}
+  // Prefiero usar esta sintaxis para la funcion en lugar de una funcion anonima.la funcion de filto debe estar dentro de la funcion del botton.onclick
+  function filtro(productos, texto){
+    return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
+    
+  };
+};
 
-const filtrado = (productos = [], texto) => {
-  return productos.filter(item => item.tipo.includes(texto) || item.color.includes(texto));
-}  
+
+};
+
+displayProductos(productos); // faltaba invocar a la funcion con el parametro de productos.
+
